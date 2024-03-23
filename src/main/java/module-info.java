a05d643b19517296611b793ed2b1efb35cacbac2
Đoàn Quang Anh <111640755+devanhdq@users.noreply.github.com>
2024-03-23 11:21:07 +0700

    initial project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.app.appbanhang {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.appbanhang to javafx.fxml;
+    exports com.appbanhang;
+}
